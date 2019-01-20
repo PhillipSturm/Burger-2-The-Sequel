7869fcd559c965adebbf1ba21e06028fe3ebd3f5
Phillip Sturm <sturmphillip95@gmail.com>
2019-01-20 16:53:36 -0600

    devour function

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,3 +1,12 @@
-// I am having trouble with my devour routes and cant figure it out right now. //
-// There needs to be a delete method targeting the id chosen and adding the name from the form to say "X ate the __ burger"
-
+$(".devour-form").on("submit", function(event) {
+    event.preventDefault();
+  
+    id = this.elements[0].value;
+  
+    //   alert($(this).id);
+    $.post("burgers/update/" + id, {
+      id: id
+    }).then(function(data) {
+      window.location.replace(data);
+    });
+  });
